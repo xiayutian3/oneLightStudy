91c7e2159e40575931f7ea0a2f2f6ea9c0a7fecf
1127071993@qq.com <aini2009626>
2021-02-26 20:20:57 +0800

    添加catch方法捕获错误

--- code/06-EcmaScript6入门及ES7-10新特性快速掌握/06-11.手写Promise A+规范代码/index.js
+++ code/06-EcmaScript6入门及ES7-10新特性快速掌握/06-11.手写Promise A+规范代码/index.js
@@ -6,83 +6,42 @@ const FULFILLED = "FULFILLED"
 const REJECTED = "REJECTED"
 
 //promise处理过程
-// function promiseResolutionProcedure(promise2,x,resolve,reject){
-//   // 处理循环引用promise
-//   if(promise2 === x){
-//     throw new Error("循环引用promise")
-//   }
-
-//   //处理promise 对象
-//   if(x instanceof MyPromise){
-//     if(x.state === PENDING){
-//       x.then(y=>{
-//         // console.log("y",y) //step6.1"
-//         // resolve(y)
-//         promiseResolutionProcedure(promise2,y,resolve,reject) //递归调用
-//       },reject) //失败就调用reject
-//     }else{
-//       //状态已经确定
-//       x.state === FULFILLED && resolve(x.value)
-//       x.state === REJECTED && reject(x.value)
-//     }
-//   }
-//   // 判断thenable 对象
-//   if((typeof x === "object" || typeof x === "function")&& x!==null){ //typeof null 是object，排除掉null
-//     if(typeof x.then === "function"){
-//       x.then(y=>{
-//         // console.log("y",y) //step6.1"
-//         // resolve(y)
-//         promiseResolutionProcedure(promise2,y,resolve,reject) //递归调用
-//       },reject) //失败就调用reject
-//     }else{
-//       resolve(x)
-//     }
-//   }else{
-//     resolve(x)
-//   }
-// }
-
-
-
-
-function promiseResolutionProcedure(promise, x, resolve, reject){
-  if (x === promise) {
-    // If promise and x refer to the same object, reject promise with a TypeError as the reason.
-    reject(new TypeError('循环引用'))
+function promiseResolutionProcedure(promise2,x,resolve,reject){
+  // 处理循环引用promise
+  if(promise2 === x){
+    throw new Error("循环引用promise")
   }
-  // if x is an object or function,
-  if (x !== null && typeof x === 'object' || typeof x === 'function') {
-    // If both resolvePromise and rejectPromise are called, or multiple calls to the same argument are made, the first call takes precedence, and any further calls are ignored.
-    let called
-    try { // If retrieving the property x.then results in a thrown exception e, reject promise with e as the reason.
-      let then = x.then // Let then be x.then
-      // If then is a function, call it with x as this
-      if (typeof then === 'function') {
-        // If/when resolvePromise is called with a value y, run [[Resolve]](promise, y)
-        // If/when rejectPromise is called with a reason r, reject promise with r.
-        then.call(x, y => {
-          if (called) return
-          called = true
-          resolvePromise(promise, y, resolve, reject)
-        }, r => {
-          if (called) return
-          called = true
-          reject(r)
-        })
-      } else {
-        // If then is not a function, fulfill promise with x.
-        resolve(x)
-      }
-    } catch (e) {
-      if (called) return
-      called = true
-      reject(e)
+
+  //处理promise 对象
+  if(x instanceof MyPromise){
+    if(x.state === PENDING){
+      x.then(y=>{
+        // console.log("y",y) //step6.1"
+        // resolve(y)
+        promiseResolutionProcedure(promise2,y,resolve,reject) //递归调用
+      },reject) //失败就调用reject
+    }else{
+      //状态已经确定
+      x.state === FULFILLED && resolve(x.value)
+      x.state === REJECTED && reject(x.value)
     }
-  } else {
-    // If x is not an object or function, fulfill promise with x
+  }
+  // 判断thenable 对象
+  if((typeof x === "object" || typeof x === "function")&& x!==null){ //typeof null 是object，排除掉null
+    if(typeof x.then === "function"){
+      x.then(y=>{
+        // console.log("y",y) //step6.1"
+        // resolve(y)
+        promiseResolutionProcedure(promise2,y,resolve,reject) //递归调用
+      },reject) //失败就调用reject
+    }else{
+      resolve(x)
+    }
+  }else{
     resolve(x)
   }
 }
+
 
 
 class MyPromise {
@@ -159,7 +118,7 @@ class MyPromise {
    
   }
   // then(onFulfilled= val=>val,onRejected = err=>{throw new Error(err)} ) {  //处理空then方法，传什么返回什么   err=>{throw new Error(err)}
-  then(onFulfilled= val=>val,onRejected) {  //处理空then方法，传什么返回什么   err=>{throw new Error(err)}
+  then(onFulfilled= val=>val,onRejected = err=>{throw err} ) {  //处理空then方法，传什么返回什么   err=>{throw new Error(err)}
   let promise2 = null
   // 处理已完成的promise（成功）
   if (this.state === FULFILLED) {
@@ -213,21 +172,8 @@ class MyPromise {
     }
   }
   catch(onRejected){
-    // debugger
-    if (this.state === PENDING) { //PENDING状态才添加
-      //缓存起来，等resolve的时候调用
-      // this.resolveCallbacks.push(onFulfilled)
-       const promise3 =  new MyPromise((resolve,reject)=>{
-        //失败
-        this.rejectedCallbacks.push(()=>{
-          // console.log(1234556)
-          const x = onRejected(this.value)  //return "step11" 返回一个新的promise
-          promiseResolutionProcedure(promise3,x,resolve,reject)
-        })
-      })
+    return this.then(null,onRejected)
 
-      return promise3
-    }
   }
   
 }
