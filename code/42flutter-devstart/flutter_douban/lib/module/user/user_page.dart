7a530dd395fa92faa3d4115082d9a1b0fce46024
1127071993@qq.com <aini2009626>
2021-05-04 21:47:18 +0800

    44-1.滚动组件

--- code/42flutter-devstart/flutter_douban/lib/module/user/user_page.dart
+++ code/42flutter-devstart/flutter_douban/lib/module/user/user_page.dart
@@ -1,16 +1,78 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class UserPage extends StatefulWidget{
+class UserPage extends StatefulWidget {
   @override
   _UserPageState createState() => _UserPageState();
-  
 }
-class _UserPageState extends State<UserPage>{
+
+class _UserPageState extends State<UserPage> {
+  ScrollController _controller = ScrollController();
+  bool _isShow = false;
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      // print(_controller.offset);
+
+      if (_controller.offset < 300) {
+        setState(() {
+          _isShow = false;
+        });
+      } else {
+        setState(() {
+          _isShow = true;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('this is user page'),
+    return Scaffold(
+      appBar: AppBar(title: Text('this is user page')),
+      // body:GridView.count(   //GridView.count 简写的方式
+      //   crossAxisCount: 4,
+      //   childAspectRatio: 1.0,
+      // // GridView(  //不简写的形式
+      // //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      // //       crossAxisCount: 3, childAspectRatio: 1.0),
+      //   children: <Widget>[
+      //     Icon(Icons.ac_unit),
+      //     Icon(Icons.ac_unit_sharp),
+      //     Icon(Icons.access_alarms_rounded),
+      //     Icon(Icons.alarm),
+      //   ],
+      // ),
+
+      body: ListView.builder(
+          //列表
+          controller: _controller,
+          itemCount: 30,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(title: Text('this is list$index'));
+          }),
+          floatingActionButton: _isShow?FloatingActionButton(  //悬浮按钮
+            child: Icon(Icons.arrow_upward),
+            onPressed: (){
+              _controller.animateTo(0, duration: Duration(milliseconds:500), curve: Curves.ease);
+            }
+          ):null,
+
+      // body: ListView.separated( //列表，带分割线
+      //   itemCount: 20,
+      //   itemBuilder: (BuildContext context,int index){
+      //     return ListTile(
+      //       title:Text('this is list$index')
+      //     );
+      //   },
+      //   separatorBuilder: (BuildContext context,int index){ //分割线
+      //     return Container(
+      //       height: 1,
+      //       color: Colors.blueAccent,
+      //     );
+      //   },
+      // ),
     );
   }
-  
 }
